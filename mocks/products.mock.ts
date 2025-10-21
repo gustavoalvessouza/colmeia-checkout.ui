412d47134ebca22431287ba33da19a731bed8199
Gustavo Alves de Souza <gustavo-gustavo20@hotmail.com>
2025-10-21 09:16:35 -0300

    feat: updated readme and organized routes

---
+++ mocks/products.mock.ts
@@ -0,0 +1,20 @@
+export const productsMock = [
+    {
+        id: 1,
+        title: "Product 1",
+        price: 10.0,
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 2,
+        title: "Product 2",
+        price: 20.0,
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 3,
+        title: "Product 3",
+        price: 30.0,
+        image: "https://via.placeholder.com/150",
+    },
+];
